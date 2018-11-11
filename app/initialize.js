53fe5f68cbc3ae81b6556a7c84d3373299749626
Sean Morris <noreply@seanmorr.is>
2018-11-11 03:07:13 -0500

    Cleanup.

--- app/initialize.js
+++ app/initialize.js
@@ -1,27 +1,27 @@
+import { Bindable } from 'curvature/base/Bindable';
 import { View     } from 'curvature/base/View';
 import { RuleSet  } from 'curvature/base/RuleSet';
 import { Persist  } from 'curvature/base/Persist';
 
 RuleSet.add('[task]', (tag)=>{
-	let r        = new View();
-	r.del        = () => {
-		r.parent.parent.args.list.splice(r.args.i, 1);
-	};
-	r.template   = `
+	console.log(tag);
+	let r      = new View();
+	r.del      = (id) => tag.parent.del(id);
+	r.template = `
 		[[i]]
-		<input cv-bind = "item.done" type = "checkbox" value = "done" />
-		<input cv-bind = "item.label" />
-		<button cv-on = "click:del(event)">-</button>
+		<input  cv-bind = "item.done" type = "checkbox" />
+		<input  cv-bind = "item.label" />
+		<button cv-on   = "click:del(i)">-</button>
 	`;
 
 	return r;
 });
 
 RuleSet.add('body', (tag)=>{
-	let r        = new View();
-	r.args.list  = [];
-	let p        = new Persist('tast-list', r.args.list);
-	r.template   = `
+	let r       = new View();
+	r.args.list = [];
+	let p       = new Persist('task-list', r.args.list);
+	r.template  = `
 		<button cv-on = "click:pre(event)">&lt;</button>
 		<button cv-on = "click:add(event)">+</button>
 		<div cv-each = "list:item:i">
@@ -29,10 +29,9 @@ RuleSet.add('body', (tag)=>{
 		</div>
 	`;
 
-	let newRecord = () => {return {label:'', done:false}};
-
-	r.pre = (event) => r.args.list.unshift(newRecord());
-	r.add = (event) => r.args.list.push(newRecord());
+	r.pre = (event) => r.args.list.unshift({label:'', done: 0});
+	r.add = (event) => r.args.list.push({label:'', done: 0});
+	r.del = (id)    => r.args.list.splice(id, 1);
 
 	return r;
 });
